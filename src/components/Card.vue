62a90eb3873a2b5085c4c324afccc8f073767961
ewgene <ewgene.shure@gmail..com>
2024-10-27 18:58:34 +0700

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <img :src="card.img">
+        </div>
+        <div class="card-body">
+            <h3>{{ card.title }}</h3>
+            <p>{{ card.description }}</p>
+            <div class="options">
+                <p class="formFactor">Pack of{{ card.formFactor}}</p>
+                <div class="order">
+                    <button @click="order -= 1">-</button>
+                    <span>{{ order }}</span>
+                    <button @click="order += 1">+</button>
+                </div>
+                <p>${{ card.price }}</p>
+            </div>
+        </div>
+        <div class="card-footer">
+            <button @click="addToCart">Add to cart</button>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent, defineProps, ref, onMounted } from 'vue'
+
+export default defineComponent({
+    props: {
+        card: {
+            img: String,
+            title: String,
+            description: String,
+            formFactor: Number,
+            price: Number,
+        }
+    },
+    setup(props) {
+        
+        const order = ref(0)
+
+        onMounted(() => {
+            JSON.parse(JSON.stringify(props))
+        })
+        return {
+            order
+        }
+    }    
+})
+
+</script>
+<style lang="scss">
+.card {
+    width: 480px;
+    height: 687px;
+    display: inline-block;
+
+    .card-header {
+        width: 480px;
+        height: 450px;
+    }
+}
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <h6 class="decor">
+        &#9472 patisserie &#9472
+      </h6>
+      &nbsp;
+      <h1>Lorem Ipsum</h1>
+      <p class="comment">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+    </div>
+  <ul class="gallery flex-container">
+    <li v-for="item in cards"
+      class="card">
+
+      <Card :card = item />
+
+    </li>
+  </ul>
+</div>
+</template>
+<script setup>
+import Card from './Card.vue'
+import { dish_1 } from './lib/dish_1.json'
+
+const cards = [
+  {
+    img: dish_1,
+    title: "Pain au Cranberry",
+    description: "A delightful spiral pastry filled with light French vanilla cream and studded with seasonal berries.",
+    formFactor: 6,
+    price: 128   
+  },
+  {
+    img: dish_1,
+    title: "Pain au Cranberry",
+    description: "A delightful spiral pastry filled with light French vanilla cream and studded with seasonal berries.",
+    formFactor: 6,
+    price: 128   
+  },
+  {
+    img: dish_1,
+    title: "Pain au Cranberry",
+    description: "A delightful spiral pastry filled with light French vanilla cream and studded with seasonal berries.",
+    formFactor: 6,
+    price: 128   
+  },
+  {
+    img: dish_1,
+    title: "Pain au Cranberry",
+    description: "A delightful spiral pastry filled with light French vanilla cream and studded with seasonal berries.",
+    formFactor: 6,
+    price: 128   
+  },
+  {
+    img: dish_1,
+    title: "Pain au Cranberry",
+    description: "A delightful spiral pastry filled with light French vanilla cream and studded with seasonal berries.",
+    formFactor: 6,
+    price: 128   
+  }
+]
+
+</script>
+<style lang="scss">
+.container {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+
+  .content {
+    display: inline-block;
+    width: 960px;
+    height: 100%;
+    position: unset;
+  }
+  .gallery {
+    width: 100%;
+    height: 687px;
+    display: flex;
+    flex-direction: row;
+    gap: 32px;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    overflow: hidden;
+
+    .card {
+      display: inline-block;
+    }
+    
+  }
+}
+</style>
